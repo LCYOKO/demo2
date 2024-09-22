87c940bd9aa9ec42b11630c116823c74a7ae1449
liu <m15879038981@163.com>
2024-09-22 16:28:24 +0800

    add

--- test.c
+++ test.c
@@ -2,7 +2,40 @@
 #include <math.h>
 #include "max.h"
 
-// void testLoop(int count);
+struct Student
+{
+    char name[100];
+    int age;
+};
+
+
+void testLoop(int count);
+
+void swap(int array[]){
+    int temp = array[0];
+    array[0] = array[1];
+    array[1] = temp;
+}
+
+void testSwap(){
+    int array[2];
+    array[0] = 1;
+    array[1] = 2;
+    printf("array[0]: %d, array[1]: %d\n",array[0],array[1]);
+    swap(array);
+    printf("array[0]: %d, array[1]: %d \n",array[0],array[1]);
+}
+
+void testString(){
+    char tsr[]="hello world";
+    printf("testString: %s. size: %d\n", tsr, sizeof(tsr));
+}
+
+void testStruct(){
+   struct Student student;
+   student.age = 12;
+   printf("testStruct: age: %d\n", student.age);
+}
 
 int main(int argc, char const *argv[]){
 
@@ -14,6 +47,8 @@ int main(int argc, char const *argv[]){
     printf("max: %d \n",max(a,b));
     printf("sqrt: %f \n", sqrt(2));
     testLoop(10);
+    testSwap();
+    testString();
     return 0;
 }
 
@@ -21,6 +56,9 @@ int main(int argc, char const *argv[]){
 void testLoop(int count){
     while(count>0){
         printf("count: %d\n",count);
+        count--;
     }
 }
+
+
 
